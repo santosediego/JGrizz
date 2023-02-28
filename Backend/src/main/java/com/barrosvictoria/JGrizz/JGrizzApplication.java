fc445c17291b5c9863cd0cf64e1aadc436f08bf0
victoria Barros <lis.victoriab@gmail.com>
2023-02-28 19:42:23 -0300

    First Commit Backend

---
+++ Backend/src/main/java/com/barrosvictoria/JGrizz/JGrizzApplication.java
@@ -0,0 +1,13 @@
+package com.barrosvictoria.JGrizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JGrizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JGrizzApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/barrosvictoria/JGrizz/JGrizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barrosvictoria.JGrizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JGrizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
