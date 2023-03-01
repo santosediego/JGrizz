c9bc8f7663c51f040f450006400f374eb1130eb6
victoria Barros <lis.victoriab@gmail.com>
2023-03-01 19:00:52 -0300

    Adding SEED

---
+++ Backend/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO TB_CLIENTS (EMAIL, NAME, NETWORK, PHONE) VALUES ('pedro@email.com', 'Pedro Gomes', '@pgomes', '34985647895');
+INSERT INTO TB_CLIENTS (EMAIL, NAME, NETWORK, PHONE) VALUES ('maria@email.com', 'Maria Souza', '@msouza', '11965419872');
+INSERT INTO TB_CLIENTS (EMAIL, NAME, NETWORK, PHONE) VALUES ('lucas@email.com', 'Lucas Silva', '@lsilva', '21932497856');
+INSERT INTO TB_CLIENTS (EMAIL, NAME, NETWORK, PHONE) VALUES ('gabriela@email.com', 'Gabriela Jesus', '@gjesus', '11996548721');
+INSERT INTO TB_CLIENTS (EMAIL, NAME, NETWORK, PHONE) VALUES ('juliana@email.com', 'Juliana Alves', '@jalves', '14987569872');
+INSERT INTO TB_CLIENTS (EMAIL, NAME, NETWORK, PHONE) VALUES ('gabriel@email.com', 'Gabriel Bernardes', '@gbernardes', '21987563218');
